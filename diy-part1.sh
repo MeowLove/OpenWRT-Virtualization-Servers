3ac4287411cca04793c36873398e97e8721abc48
MeowLove <32319912+MeowLove@users.noreply.github.com>
2021-03-24 16:58:34 +0800

    Version v0.0.4
    测试编译，重建配置文件

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,13 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Install Environment
+sudo apt -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl swig rsync mkisofs
+
+# Update Themes to Argon
+cd package/lean
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+cd ..
+cd ..

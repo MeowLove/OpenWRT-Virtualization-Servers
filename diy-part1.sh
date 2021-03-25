5acd7ea561e975e5138251eec94acbc016e22976
MeowLove <32319912+MeowLove@users.noreply.github.com>
2021-03-25 14:01:58 +0800

    Version v0.2.1
    更新主题色为快乐粉，满足诸位的少女心

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,10 +22,12 @@ echo "Start CXT Setting the firmware 1"
 # Install Environment
 sudo apt -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl swig rsync mkisofs
 
-# Update Themes to Argon
+# Update Themes to Argon and Modify Color
 cd package/lean
 rm -rf luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+sed -i "s/--primary: #5e72e4/--primary: #ff26d6d1/g" luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
+sed -i "s/--primary: #5e72e4/--primary: #ff26d6d1/g" luci-theme-argon/htdocs/luci-static/argon/less/cascade.less
 cd ..
 cd ..
 
